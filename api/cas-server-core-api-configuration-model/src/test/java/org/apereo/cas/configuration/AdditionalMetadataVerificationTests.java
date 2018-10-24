9c75e414ab2ed5178ecb708c84103df9496b8232
Timur Duehr <tduehr@gmail.com>
2018-10-23 23:06:07 -0500

    spring rules

--- api/cas-server-core-api-configuration-model/src/test/java/org/apereo/cas/configuration/AdditionalMetadataVerificationTests.java
+++ api/cas-server-core-api-configuration-model/src/test/java/org/apereo/cas/configuration/AdditionalMetadataVerificationTests.java
@@ -6,16 +6,14 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.configurationmetadata.ConfigurationMetadataProperty;
 import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
 import org.springframework.boot.context.properties.source.InvalidConfigurationPropertyNameException;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.io.IOException;
 import java.util.Map;
@@ -28,13 +26,8 @@ import static org.junit.Assert.*;
  *
  * @since 6.0
  */
+@SpringBootTest(classes = AopAutoConfiguration.class)
 public class AdditionalMetadataVerificationTests {
-
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @Autowired
     private ResourceLoader resourceLoader;
@@ -47,15 +40,18 @@ public class AdditionalMetadataVerificationTests {
      * @throws IOException if additional property file is missing
      */
     @Test
+    @SuppressWarnings("unchecked")
     public void verifyMetaData() throws IOException {
         val mapper = new ObjectMapper().findAndRegisterModules();
-        val jsonFile = resourceLoader.getResource("META-INF/additional-spring-configuration-metadata.json");
+        val jsonFile = resourceLoader.getResource(
+            CasConfigurationProperties.class.getClassLoader().getResource("META-INF/additional-spring-configuration-metadata.json").toString()
+        );
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
         mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
         val values = new TypeReference<Map<String, Set<ConfigurationMetadataProperty>>>() {
         };
-        val jsonMap = (Map) mapper.readValue(jsonFile.getURL(), values);
-        val props = (Set<ConfigurationMetadataProperty>) jsonMap.get("properties");
+        val jsonMap = (Map<String, Set<ConfigurationMetadataProperty>>) mapper.readValue(jsonFile.getURL(), values);
+        val props = jsonMap.get("properties");
         for (val prop : props) {
             try {
                 ConfigurationPropertyName.of(prop.getName());
