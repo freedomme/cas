b0884638c0cd1453afff96e067b02bac28ef5b95
Timur Duehr <tduehr@gmail.com>
2018-10-25 08:26:31 -0500

    org.junit.Assert to Assertions
    and remove assertj
    reduce hamcrest

--- api/cas-server-core-api-configuration-model/src/test/java/org/apereo/cas/configuration/AdditionalMetadataVerificationTests.java
+++ api/cas-server-core-api-configuration-model/src/test/java/org/apereo/cas/configuration/AdditionalMetadataVerificationTests.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test additional metadata validity.
